6de99de128ae5feb734f5ef2b60c4ab4761c3917
Worldsalt3 <riliwanhazzan@gmail.com>
2022-10-30 10:44:54 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const Joi = require('joi')
+
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' },
+]
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.get('/api/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id))
+
+  if (!course) {
+    console.log(course)
+    res.status(404).send('The course with the given ID not found')
+  }
+  res.send(course)
+})
+
+app.post('/api/courses', (req, res) => {
+
+const schema = {
+    name: Joi.string().min(3).required()
+}
+
+const result = Joi.validate(req.body, schema)
+
+
+if(result.error) {
+    res.status(400).send(result.error.details[0].message)
+    return
+}
+
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  }
+  courses.push(course)
+  res.send(course)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`listening on port ${port}...`))
