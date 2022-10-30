f795e3c4a659e676a676371acd02898278baaab5
Worldsalt3 <riliwanhazzan@gmail.com>
2022-10-30 21:41:05 +0100

    learnt about RESTFUL api

--- app.js
+++ app.js
@@ -23,27 +23,16 @@ app.get('/api/courses', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id))
 
-  if (!course) {
-    console.log(course)
-    res.status(404).send('The course with the given ID not found')
-  }
+  if (!course) return res.status(404).send('The course with the given ID not found')
+  
   res.send(course)
 })
 
 app.post('/api/courses', (req, res) => {
+  const { error } = validateCourse(req.body)
 
-const schema = {
-    name: Joi.string().min(3).required()
-}
-
-const result = Joi.validate(req.body, schema)
-
-
-if(result.error) {
-    res.status(400).send(result.error.details[0].message)
-    return
-}
-
+  if (error) return res.status(400).send(error.details[0].message)
+       
 
   const course = {
     id: courses.length + 1,
@@ -51,6 +40,36 @@ if(result.error) {
   }
   courses.push(course)
   res.send(course)
+})
+
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id))
+  if (!course) return res.status(404).send('The course with the given ID was not found')
+
+  const { error } = validateCourse(req.body)
+
+  if (error) return res.status(400).send(error.details[0].message)
+
+  course.name = req.body.name
+  res.send(course)
+})
+
+const validateCourse = (course) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  }
+
+  return Joi.validate(course, schema)
+}
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if(!course) return res.status(404).send('The course with the given ID was not found')
+
+    const index = courses.indexOf(course)
+    courses.splice(index, 1)
+
+    res.send(course)
 })
 
 const port = process.env.PORT || 3000
