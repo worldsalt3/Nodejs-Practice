25e9a43532ee5ebd3b11271c2efbf6366729e77c
Worldsalt3 <riliwanhazzan@gmail.com>
2022-10-31 12:01:51 +0100

    learnt about template engine using pug and also restructure project files

--- app.js
+++ app.js
@@ -3,30 +3,34 @@ const dbDebugger = require('debug')('app:db')
 
 const config = require('config')
 const Joi = require('joi')
-const logger = require('./logger')
-const auth = require('./auth')
+const logger = require('./middleware/logger')
+const auth = require('./middleware/auth')
 const helmet = require('helmet')
 const morgan = require('morgan')
+const courses = require('./routes/courses')
+const home = require('./routes/home')
 const express = require('express')
 
 const app = express()
+
+app.set('view engine', 'pug') //declares path
+app.set('views', './views') //path
 
 // configuration
 console.log(`Aplication name: ${config.get('name')}`)
 console.log(`Mail server name: ${config.get('mail.host')}`)
 console.log(`password of mail server: ${config.get('mail.password')}`)
 
-
-
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(helmet())
+app.use('/api/courses', courses)
+app.use('/', home)
 
 if (app.get('env') === 'development') {
-    app.use(morgan('tiny'))
-    startupDebugger('morgan enabled...')
+  app.use(morgan('tiny'))
+  startupDebugger('morgan enabled...')
 }
 
 // db work
@@ -35,74 +39,6 @@ dbDebugger('connected to the database...')
 app.use(logger)
 
 app.use(auth)
-
-const courses = [
-  { id: 1, name: 'course1' },
-  { id: 2, name: 'course2' },
-  { id: 3, name: 'course3' },
-]
-
-app.get('/', (req, res) => {
-  res.send('Hello')
-})
-
-app.get('/api/courses', (req, res) => {
-  res.send(courses)
-})
-
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id))
-
-  if (!course)
-    return res.status(404).send('The course with the given ID not found')
-
-  res.send(course)
-})
-
-app.post('/api/courses', (req, res) => {
-  const { error } = validateCourse(req.body)
-
-  if (error) return res.status(400).send(error.details[0].message)
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  }
-  courses.push(course)
-  res.send(course)
-})
-
-app.put('/api/courses/:id', (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id))
-  if (!course)
-    return res.status(404).send('The course with the given ID was not found')
-
-  const { error } = validateCourse(req.body)
-
-  if (error) return res.status(400).send(error.details[0].message)
-
-  course.name = req.body.name
-  res.send(course)
-})
-
-const validateCourse = (course) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  }
-
-  return Joi.validate(course, schema)
-}
-
-app.delete('/api/courses/:id', (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id))
-  if (!course)
-    return res.status(404).send('The course with the given ID was not found')
-
-  const index = courses.indexOf(course)
-  courses.splice(index, 1)
-
-  res.send(course)
-})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on port ${port}...`))
---
+++ routes/home.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'My Express App',
+    message: 'Welcome to my first express app',
+  })
+})
+
+
+module.exports = router
